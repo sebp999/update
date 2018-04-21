9b67b50bbe1b5f67766a522eaa137008c66a75c4
sebp999 <sebswebs@gmail.com>
2018-04-21 10:58:16 +0100

    added jquery and bulma

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 $( document ).ready(function() {
-    console.log("ready");
-    $('#proc').click(
-      function(){
-        console.log('click');
+  // $('.fa-check').css('visibility','hidden');
+  $('#click1').click(function(){
+    $('#download .fa-spinner').hide();
+    $('#download .fa-check').css('visibility','visible').hide().fadeIn('slow');
 
-        $('#download').addClass('fa-pulse');
-      });
+    $('#download').animate({color:'green'});
+    $('#open .fa-spinner').css('visibility','visible').hide().fadeIn('fast');
+    //$('#download').css("color",'green');
+
+  });
+  $('#click2').click(function(){
+    $('#open .fa-spinner').hide();
+    $('#open .fa-check').css('visibility','visible').hide().fadeIn('slow');
+
+    $('#open').animate({color:'green'});
+    //$('#download').css("color",'green');
+    $('#read .fa-spinner').css('visibility','visible').hide().fadeIn('fast');
+
+  });
 });
